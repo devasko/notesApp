8439fa0226a11a439de933f646d47e290480bad4
Alex Korobov <devasko@ro.ru>
2021-05-05 22:17:50 +0300

    Вынесены компоненты заметок, сообщений+стили, пременныsе scss

---
+++ src/components/Message.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <!-- Вывод сообщений -->
+    <div class="message">
+        <p>{{ message }}</p>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        props: {
+            message: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .message {
+        padding: 15px;
+        text-align: center;
+
+        p {
+            color: #ba3838;
+        }
+    }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import NewNote from './components/NewNote'
+import './assets/scss/main.scss'
+
+createApp(App).mount('#app');
